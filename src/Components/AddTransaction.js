0cecc32ca23d02655306e6c7b6a9ad4bf24507f7
M-Hammad0 <hammad_205@hotmail.com>
2020-06-14 02:36:43 +0500

    add state to transaction

--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../Context/GlobalState";
 
 export default function AddTransaction() {
+      
       const [text, setText] = useState("");
-      const [amount, setAmount] = useState(0.00);     
+      const [amount, setAmount] = useState(0.00);
+      
+      const {addTransaction} = useContext(GlobalContext);
+
+      const onSubmit = event => { 
+        event.preventDefault();
+
+        const newTransaction = {
+          id : Math.floor(Math.random() * 100000),
+          text,
+          amount: +amount
+        }
+
+        addTransaction(newTransaction);
+      }
+
+
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(event) => setText(event.target.value) } placeholder="Enter text..." />
--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React,{useContext} from "react";
+import { GlobalContext } from "../Context/GlobalState";
 
 export default function Transaction(props) {
+      const {deleteTransaction} = useContext(GlobalContext);
       const sign = props.trans.amount > 0 ? '+' : '-';
   return (
       <li className={props.trans.amount > 0 ? "plus": "minus"}>
       {props.trans.text} <span>{sign}{Math.abs(props.trans.amount)}</span>
-      <button className="delete-btn">x</button>
+      <button onClick={() => deleteTransaction(props.trans.id)} className="delete-btn">x</button>
     </li>
   );
 }
